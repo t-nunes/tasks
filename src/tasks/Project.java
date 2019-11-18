59ef425065bc96924a0947958b521aa7c3ea45f7
Diego Ramos Gomes <diegoramosgomes5@gmail.com>
2019-11-18 20:04:34 -0300

    Adicionado project

---
+++ src/tasks/Project.java
@@ -0,0 +1,19 @@
+package tasks;
+
+public class Project {
+    private int id;
+    private String name;
+
+    public Project(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -1,7 +1,31 @@
 package tasks;
 
 public class Task {
-    public static void x() {
-        System.out.println(" sadasdasd");
+    private int id;
+    private String description;
+    private User user;
+    private Project project;
+
+    public Task(int id, String description, User user, Project project) {
+        this.id = id;
+        this.description = description;
+        this.user = user;
+        this.project = project;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Project getProject() {
+        return project;
     }
 }
