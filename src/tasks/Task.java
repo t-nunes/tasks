cdabd84da223deeb8bbcc871fa3ff6036ff3a743
Diego Ramos Gomes <diegoramosgomes5@gmail.com>
2019-11-18 20:10:36 -0300

    Corrigido Task ter apenas um usuario

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -1,16 +1,27 @@
 package tasks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task {
     private int id;
     private String description;
-    private User user;
+    private List<User> users;
     private Project project;
 
-    public Task(int id, String description, User user, Project project) {
+    public Task(int id, String description, Project project) {
         this.id = id;
         this.description = description;
-        this.user = user;
         this.project = project;
+        users = new ArrayList<>();
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public List<User> getUsers() {
+        return users;
     }
 
     public int getId() {
@@ -19,10 +30,6 @@ public class Task {
 
     public String getDescription() {
         return description;
-    }
-
-    public User getUser() {
-        return user;
     }
 
     public Project getProject() {
